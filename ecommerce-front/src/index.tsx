e61ba76eb1fd2114e9528232e3acc6eed0c11edf
wal71225-git <2082322652@qq.com>
2021-03-14 22:30:43 +0800

    路由初始化

--- ecommerce-front/src/index.tsx
+++ ecommerce-front/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { API } from './config';
+console.log(API)
 ReactDOM.render(
   <React.StrictMode>
     <App />
