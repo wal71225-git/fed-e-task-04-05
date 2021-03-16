40e32f6da9b57e597bffe1760b7c298dcf93f773
mac <a1234@1234deMacBook-Pro.local>
2021-03-16 14:37:33 +0800

    feat:jsx报错解决

--- ecommerce-front/src/components/core/Navigation.tsx
+++ ecommerce-front/src/components/core/Navigation.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
-import Layout from './Layout'
 import { Menu } from 'antd'
 import { Link } from 'react-router-dom'
 const Navigation = () => {
     return (
-      <Layout>
         <Menu mode="horizontal">
           <Menu.Item>
             <Link to="/">首页</Link>
           </Menu.Item>
           <Menu.Item>
-            <Link to="/Shop">商城</Link>
+            <Link to="/Shop">商城222</Link>
           </Menu.Item>
         </Menu>
-      </Layout>
     )
 }
 export default Navigation
